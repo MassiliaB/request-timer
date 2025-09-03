ccb1203f0869aea776108841c4ab65b76200cdfe
Massilia <massblri@gmail.com>
2025-09-03 12:35:32 +0200

    adjust some errors

--- request_timer.py
+++ request_timer.py
@@ -4,58 +4,58 @@ import time
 import argparse
 
 def load_payloads(files):
-	payload_set = []
+	payload_sets = []
 	for f in files:
 		with open(f, "r") as infile:
 			payloads = [line.strip() for line in infile if line.strip()]
 			payload_sets.append(payloads)
 	return payload_sets
 
-def substitute_placehoders(text, payloads, i):
-    if not text:
-        return text
-    for j, payload_list in enumerate(payloads, start=1):
-        placeholders = f"§{j}§"
-    if i < len(payload_list):
-        text = text.replace(placeholder, payload_list[i])
-    return text
+def substitute_placeholders(text, payloads, i):
+	if not text:
+		return text
+	for j, payload_list in enumerate(payloads, start=1):
+		placeholder = f"§{j}§"
+		if i < len(payload_list):
+			text = text.replace(placeholder, payload_list[i])
+	return text
 
-def pitchfork_attack(url, body, payload_set, headers_list=None, method="POST"):
+def pitchfork_attack(url, body, payload_sets, headers_list=None, method="POST"):
 	nb_requests = min(len(p) for p in payload_sets)
 
-    for i in range(nb_requests):
-        body = substitute_placeholders(body, payload_set, i)
+	for i in range(nb_requests):
+		req_body = substitute_placeholders(body, payload_sets, i)
 
-        headers = {}
-	    if headers_list:
-		    for h in headers_list:
-		    k, v = h.split(":", 1)
-		    k = substitute_placeholders(k.strip(), payload_set,i)
-		    v = substitute_placeholders(v.strip(), payload_set,i)
-            headers[k] = v
+		headers = {}
+		if headers_list:
+			for h in headers_list:
+				k, v = h.split(":", 1)
+				k = substitute_placeholders(k.strip(), payload_sets, i)
+				v = substitute_placeholders(v.strip(), payload_sets, i)
+				headers[k] = v
 
-        start = time.time()
-        r = requests.request(method, url, headers=headers, data=body)
-        end = time.time()
-        duration = end - start
+		start = time.time()
+		r = requests.request(method, url, headers=headers, data=req_body)
+		end = time.time()
+		duration = end - start
 
-        current_payload = [pl[i] for pl in payload_set]
-        payload_str = " | ".join(current_payload)
+		current_payload = [pl[i] for pl in payload_sets]
+		payload_str = " | ".join(current_payload)
 
-        print(f"{payload_str} || {r.status_code} | {len(r.text)} | {duration:.4f}s")
+		print(f"{payload_str} || {r.status_code} | {len(r.text)} | {duration:.4f}s")
 
     
 
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Mini Pitchfork Intruder to measure response times")
-	parser.add_argument("url", required=True, help="Target URL")
+	parser.add_argument("url", help="Target URL")
 	parser.add_argument("-p", "--payloads", nargs="+", required=True, help="Payload files (one per §n§ placeholder accordingly)")
-	parser.add_argument("-b", "--body", help="Request body (use §n§ as placeholders)"
+	parser.add_argument("-b", "--body", help="Request body (use §n§ as placeholders)")
 	parser.add_argument("-X", "--method", default="POST", help="HTTP method (default POST)")
-	parser.add_argument("-H", "--header", action:"append", help="Custom headers (use $n$ as placeholders)")
+	parser.add_argument("-H", "--header", action="append", help="Custom headers (use §n§ as placeholders)")
 
 	args = parser.parse_args()
 
-	payload_set = load_payloads(args.payloads)
-	pitchfork_attack(args.url, args.body, payload_set, headers_list=args.header, method=args.method)
+	payload_sets = load_payloads(args.payloads)
+	pitchfork_attack(args.url, args.body, payload_sets, headers_list=args.header, method=args.method)
